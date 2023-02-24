7b11896d3fb05d5cb328b3b799e3a4fe07ab24c8
dunnie99 <oluwadunnirachael@gmail.com>
2023-02-24 19:43:28 +0100

    commit

---
+++ scripts/random.ts
@@ -0,0 +1,50 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const [owner] = await ethers.getSigners();
+  
+    const rangen = await ethers.getContractFactory("randomGenarator");
+    const randGen = await rangen.deploy();
+    await randGen.deployed();
+
+    console.log(`randomGenerator contract is ${randGen.address}`);
+
+
+
+
+
+}
+
+
+
+
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
